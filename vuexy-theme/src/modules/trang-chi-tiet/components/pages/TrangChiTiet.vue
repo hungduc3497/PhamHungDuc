86dd8d3b137dc5ab8fb46aad2747883ca32b8676
ducph <ducph@edsolabs.com>
2021-07-05 13:19:01 +0700

    update trang chi tiết

--- vuexy-theme/src/modules/trang-chi-tiet/components/pages/TrangChiTiet.vue
+++ vuexy-theme/src/modules/trang-chi-tiet/components/pages/TrangChiTiet.vue
@@ -10,24 +10,174 @@
         <span class="pl-2">RSS</span>
         <span class="pl-2">Sơ đồ</span>
         <span class="pl-3">Ngôn ngữ: Tiếng Việt </span>
-        <img src="../../assets/images/arrow_down_head.jpg" alt="">
+        <img src="../../assets/images/arrow_down_head.jpg" alt="" />
       </div>
     </div>
     <div class="d-flex align-items-center head-navbar">
+      <div>
+        <span class="pl-2">Giới thiệu</span>
+        <span class="pl-2">Bộ trưởng</span>
+        <span class="pl-2">Dịch vụ công</span>
+        <span class="pl-2">Tin tức </span>
+        <img src="../../assets/images/arrow_down_navbar.png" alt="" />
+      </div>
+      <div class="ml-auto">
+        <img src="../../assets/images/Path.png" alt="" />
+        <span> Đường dây nóng</span>
+        <img src="../../assets/images/contact_phone.png" class="pl-2" alt="" />
+        <span> Danh bạ điện thoại</span>
+        <span class="pl-2">Chỉnh cỡ chữ: A a</span>
+      </div>
+    </div>
+    <div class="d-flex">
+      <div class="mt-1">
+        <h2 class="text-content-red">
+          Bộ trưởng Nguyễn Chí Dũng làm việc với Ban Lãnh đạo mới của Diễn đàn
+          doanh nghiệp Việt Nam
+        </h2>
+        <div class="d-flex py-1">
+          <div>
+            <img src="../../assets/images/viet_tat.png" alt="" />
+            <span class="pl-1">Từ viết tắt</span>
+            <img src="../../assets/images/volume_up.png" class="ml-3" alt="" />
+            <span class="pl-1">Đọc bài viết</span>
+          </div>
+          <div class="box-style">
+            <span class="nenMauBe">Nền màu be</span>
+            <span class="nenMauTrang">Nền màu trắng</span>
+            <span class="nenMauXam">Nền màu xám</span>
+            <span class="chuMauTrang">Chữ màu trắng</span>
+            <span class="chuMauLam">Chữ màu lam</span>
+            <span class="chuMauDen">Chữ màu đen</span>
+          </div>
+        </div>
         <div>
-            <span class="pl-2">Giới thiệu</span>
-            <span class="pl-2">Bộ trưởng</span>
-            <span class="pl-2">Dịch vụ công</span>
-            <span class="pl-2">Tin tức </span>
-            <img src="../../assets/images/arrow_down_navbar.png" alt="">
+          <span style="font-size: 12px">Ngày 05/05/2021-16:30:00 PM </span>
         </div>
-        <div class="ml-auto">
-            <img src="../../assets/images/Path.png" alt="">
-            <span> Đường dây nóng</span>
-            <img src="../../assets/images/contact_phone.png" class="pl-2" alt="">
-            <span> Danh bạ điện thoại</span>
-            <span class="pl-2">Chỉnh cỡ chữ: A a</span>
+        <div class="d-flex flex-column text-content">
+          <span>
+            (MPI) – Sáng ngày 26/4/2021, tại trụ sở Bộ Kế hoạch và Đầu tư, Bộ
+            trưởng Nguyễn Chí Dũng đã có buổi làm việc với Ban Lãnh đạo mới của
+            Diễn đàn doanh nghiệp Việt Nam (VBF) để trao đổi về việc tổ chức VBF
+            năm 2021.
+          </span>
+          <div class="align-self-center">
+            <img src="../../assets/images/image.png" alt="" />
+          </div>
+          <p>
+            Tham dự buổi làm việc có Đồng Chủ tịch Liên minh VBF và Hiệp hội
+            Doanh nghiệp Nhật Bản tại Việt Nam (JCCI) Tetsu Funayama; Đại diện
+            JCCI Hirohide Sagara; Đại diện Phòng Thương mại Châu Âu tại Việt Nam
+            (EuroCham) Soren Roed Pedersen; Phó chủ tịch Hiệp hội Doanh nghiệp
+            Hàn Quốc tại Việt Nam (KoCham) Hong Sun; Đại diện Hiệp hội Thương
+            mại Hoa Kỳ (AmCham) Eric Johnson; Đại diện Hiệp hội các Doanh nghiệp
+            Anh quốc tại Việt Nam (BritCham) Chris Jeffery; Phó Chủ tịch
+            Singapore Business Group Seck Yee Chung.
+          </p>
+          <p>
+            Phát biểu tại buổi làm việc, Đồng Chủ tịch liên minh VBF và JCCI
+            Tetsu Funayama chúc mừng Bộ trưởng Nguyễn Chí Dũng đã tái đắc cử Ủy
+            viên Trung ương Đảng khóa XIII và Bộ trưởng Bộ Kế hoạch và Đầu tư
+            nhiệm kỳ 2021-2026. Đồng thời hy vọng sẽ tiếp tục được đồng hành với
+            Bộ Kế hoạch và Đầu tư trong việc xúc tiến đầu tư, quảng bá môi
+            trường đầu tư kinh doanh tại Việt Nam và kêu gọi đầu tư từ các tập
+            đoàn kinh tế lớn cũng như hỗ trợ các doanh nghiệp nhỏ và vừa trên
+            thế giới đầu tư vào Việt Nam.
+          </p>
+          <p>
+            Ông Tetsu Funayama cho biết, trong nội bộ của VBF đã thảo luận và
+            thống nhất đề xuất chủ đề chính của VBF năm 2021 là “Tiêu điểm 2025
+            - Làm thế nào để Khu vực tư nhân và Chính phủ song hành tạo nên nền
+            tảng An toàn và Bền vững để đạt các mục tiêu năm 2025”.
+          </p>
+          <div class="align-self-center">
+            <img
+              src="../../assets/images/bo_truong_1_1.png"
+              class="imgBoTruong"
+              alt=""
+            />
+          </div>
+          <p>
+            Phát biểu tại buổi làm việc, Bộ trưởng Nguyễn Chí Dũng hoan nghênh
+            sự chủ động công việc của Nhóm công tác VBF trong việc đề xuất nội
+            dung và chúc mừng Ban lãnh đạo mới của VBF nhiệm kỳ 2021. Bộ trưởng
+            đánh giá cao vai trò và đóng góp của VBF trong suốt 23 năm qua đã
+            luôn đồng hành cùng Chính phủ, các cấp, ngành và cộng đồng doanh
+            nghiệp trong việc cải thiện môi trường đầu tư kinh doanh, tháo gỡ
+            khó khăn vướng mắc, thúc đẩy đổi mới sáng tạo, khởi nghiệp, phát
+            triển doanh nghiệp. Đồng thời tin tưởng thời gian tới, VBF sẽ tiếp
+            tục là một kênh đối thoại chính sách quan trọng và hiệu quả giữa
+            cộng đồng doanh nghiệp với Chính phủ.
+          </p>
+          <p>
+            Trong chiến lược và kế hoạch phát triển kinh tế - xã hội của giai
+            đoạn tới, Việt Nam đang hướng đến các mục tiêu đẩy mạnh công nghiệp
+            hóa, hiện đại hóa, cơ cấu lại nền kinh tế gắn với đổi mới mô hình
+            tăng trưởng, bảo đảm thực chất, hiệu quả. Ưu tiên phát triển hạ tầng
+            trọng yếu về giao thông, năng lượng, công nghệ thông tin, đô thị
+            lớn, hạ tầng thích ứng với biến đổi khí hậu, tăng cường kết nối nội
+            vùng, trong nước và quốc tế. Đồng thời, phát triển và ứng dụng mạnh
+            mẽ khoa học, công nghệ và đổi mới sáng tạo nhằm tạo bứt phá nâng cao
+            năng suất, chất lượng, hiệu quả và sức cạnh tranh của nền kinh tế.
+            Chú trọng phát triển giáo dục, đào tạo, nguồn nhân lực chất lượng
+            cao đáp ứng yêu cầu của cuộc Cách mạng công nghiệp lần thứ tư và hội
+            nhập quốc tế. Do đó, định hướng phát triển của VBF thời gian tới cần
+            tập trung, ưu tiên những mục tiêu nêu trên.
+          </p>
+          <div class="ml-auto">
+            <p>Thúy Quyên</p>
+            <p><b>Bộ Kế hoạch và Đầu tư</b></p>
+            <p>
+              <span>Tổng số lượt xem: 1234</span
+              ><img src="../../assets/images/printshop.png" alt="" />
+            </p>
+          </div>
+          <div>
+            <h2 class="text-content-red">Tin bài liên quan</h2>
+            <hr />
+            <div class="text-tin-bai">
+              <p>
+                <b
+                  >Dự thảo Nghị định sửa đổi, bổ sung một số điều của Nghị định
+                  số 95/2016/NĐ-CP về xử phạt vi phạm hành chính trong lĩnh vực
+                  thống kê</b
+                >
+              </p>
+              <p>Ngày đăng: 19/04/2021</p>
+              <p>
+                <b
+                  >Dự thảo Nghị định sửa đổi, bổ sung một số điều của Nghị định
+                  số 95/2016/NĐ-CP về xử phạt vi phạm hành chính trong lĩnh vực
+                  thống kê</b
+                >
+              </p>
+              <p>Ngày bắt đầu: 19/04/2021</p>
+              <p>
+                <b
+                  >Dự thảo Nghị định sửa đổi, bổ sung một số điều của Nghị định
+                  số 95/2016/NĐ-CP về xử phạt vi phạm hành chính trong lĩnh vực
+                  thống kê</b
+                >
+              </p>
+              <p>Ngày bắt đầu: 19/04/2021</p>
+              <p>
+                <b
+                  >Dự thảo Nghị định sửa đổi, bổ sung một số điều của Nghị định
+                  số 95/2016/NĐ-CP về xử phạt vi phạm hành chính trong lĩnh vực
+                  thống kê</b
+                >
+              </p>
+              <p>Ngày bắt đầu: 19/04/2021</p>
+            </div>
+          </div>
         </div>
+      </div>
+      <div class="mt-1">
+        <h2 class="text-content-red">
+          Thông báo
+        </h2>
+        <hr>
+      </div>
     </div>
   </div>
 </template>
